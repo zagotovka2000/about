2ae9ff93a7e370e66effbe9411c56f5aaf4e15b5
unknown <epifanovvaleriy@gmail.com>
2025-04-01 17:58:42 +0300

    simple commit for commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Main from './Main/Main'
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="App-header">
+<Main/>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
